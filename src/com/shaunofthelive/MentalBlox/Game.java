f6daaa18642316e74a64afc2d1e0c32e376dab81
Shaun Hill <shaun.hill@gmail.com>
2013-10-09 20:52:19 -0400

    add constructor

--- src/com/shaunofthelive/MentalBlox/Game.java
+++ src/com/shaunofthelive/MentalBlox/Game.java
@@ -2,5 +2,12 @@ package com.shaunofthelive.MentalBlox;
 
 public class Game {
 	private Board board;
-	private Die[] die;
+	private Die[] dice;
+	
+	public Game() {
+		board = new Board();
+		dice = new Die[2];
+		dice[0] = new Die(6);
+		dice[1] = new Die(6);
+	}
 }
