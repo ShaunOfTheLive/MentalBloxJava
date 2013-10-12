faf3a4bd38ed5af15e4b4d2b8b6fb8932da2f668
Shaun Hill <shaun.hill@gmail.com>
2013-10-12 03:35:47 -0400

    getters

--- src/com/shaunofthelive/MentalBlox/Game.java
+++ src/com/shaunofthelive/MentalBlox/Game.java
@@ -1,6 +1,22 @@
 package com.shaunofthelive.MentalBlox;
 
 public class Game {
+	public Board getBoard() {
+		return board;
+	}
+
+	private void setBoard(Board board) {
+		this.board = board;
+	}
+
+	public Die[] getDice() {
+		return dice;
+	}
+
+	private void setDice(Die[] dice) {
+		this.dice = dice;
+	}
+
 	private Board board;
 	private Die[] dice;
 	
