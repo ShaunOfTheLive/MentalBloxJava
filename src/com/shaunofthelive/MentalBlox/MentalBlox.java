b45648668ae7e78ed1cdbbfec2828398729e8f5d
Shaun Hill <shaun.hill@gmail.com>
2013-10-12 04:24:44 -0400

    start implementing main

--- src/com/shaunofthelive/MentalBlox/MentalBlox.java
+++ src/com/shaunofthelive/MentalBlox/MentalBlox.java
@@ -13,8 +13,27 @@ public class MentalBlox {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Game game = new Game();
+		Board board = game.getBoard();
+		Die[] dice = game.getDice();
+		int currentPlayer = 0;
+		
+		// players roll dice to see who goes first
+		int player1roll = game.rollBothDice();
+		int player2roll = game.rollBothDice();
+		
+		while (true) {
+			if (player1roll > player2roll) {
+				currentPlayer = 1;
+				break;
+			} else if (player2roll > player1roll) {
+				currentPlayer = 2;
+				break;
+			}
+		}
+		
+		System.out.println("Player 1 rolls " + player1roll);
+		System.out.println("Player 2 rolls " + player2roll);
+		System.out.println("Player " + currentPlayer + " starts.");
 	}
-
 }
