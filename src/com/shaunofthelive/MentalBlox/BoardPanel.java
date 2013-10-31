80713664e0b832778f3e47f3471a6da89893d2f4
Shaun Hill <shaun.hill@gmail.com>
2013-10-31 01:11:40 -0400

    draw grid lines

--- src/com/shaunofthelive/MentalBlox/BoardPanel.java
+++ src/com/shaunofthelive/MentalBlox/BoardPanel.java
@@ -1,5 +1,6 @@
 package com.shaunofthelive.MentalBlox;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -25,8 +26,25 @@ public class BoardPanel extends JPanel {
 
         Graphics2D g2d = (Graphics2D) g;
 
+        BasicStroke gridStroke = new BasicStroke(
+        		5,
+        		BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_ROUND,
+                1.0f);
+        
         g2d.setColor(Color.white);
-
+        g2d.setStroke(gridStroke);
+        // horizontal lines
+        g2d.drawLine(  0,   0, 768,   0);
+        g2d.drawLine(  0, 256, 768, 256);
+        g2d.drawLine(  0, 512, 768, 512);
+        g2d.drawLine(  0, 768, 768, 768);
+        // vertical lines
+        g2d.drawLine(  0,   0,   0, 768);
+        g2d.drawLine(256,   0, 256, 768);
+        g2d.drawLine(512,   0, 512, 768);
+        g2d.drawLine(768,   0, 768, 768);
+        
     }
 
     @Override
