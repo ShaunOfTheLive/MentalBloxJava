9c7171ce4029d4719b689f885d3663eb2224be50
Shaun Hill <shaun.hill@gmail.com>
2013-11-11 21:58:23 -0500

    set window size to maximum vertical bounds and adjust position a bit

--- src/com/shaunofthelive/MentalBlox/GameWindow.java
+++ src/com/shaunofthelive/MentalBlox/GameWindow.java
@@ -1,5 +1,8 @@
 package com.shaunofthelive.MentalBlox;
 
+import java.awt.GraphicsEnvironment;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
@@ -14,14 +17,23 @@ public class GameWindow {
         
 		frame = new JFrame();
 		frame.setTitle("Mental Blox");
-		frame.setSize(768, 768);
+		
+		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		Rectangle bounds = env.getMaximumWindowBounds();
+		System.out.println("Screen Bounds: " + bounds );
+		frame.setSize(bounds.height, bounds.height);
+				
 		frame.setLocationRelativeTo(null);
+		
+		Point p = frame.getLocation();
+		frame.setLocation(new Point(p.x, p.y+5));
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		//frame.pack();
 			
 		BoardPanel boardPanel = new BoardPanel();
-		frame.add(boardPanel);	
+		frame.add(boardPanel);
      }
 	
 	public JFrame getFrame() {
