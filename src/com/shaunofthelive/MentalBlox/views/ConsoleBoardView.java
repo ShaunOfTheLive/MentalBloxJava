75321335f4564db3c5c9432e75c84f6405c063e5
Shaun Hill <shaun.hill@gmail.com>
2018-11-28 16:59:15 -0500

    print basic game/board layout

--- src/com/shaunofthelive/MentalBlox/views/ConsoleBoardView.java
+++ src/com/shaunofthelive/MentalBlox/views/ConsoleBoardView.java
@@ -1,4 +1,31 @@
 package com.shaunofthelive.MentalBlox.views;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ConsoleBoardView implements IBoardView {
+    final int ROWS = 24;
+    final int COLS = 80;
+    char[][] board = new char [ROWS][COLS];
+
+    public ConsoleBoardView() {
+        String file = "res/ConsoleBoardView.txt";
+        int i = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                board[i] = line.toCharArray();
+                ++i;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(char[][] g) {
+        for (int i = 0; i < g.length; ++i) {
+            g[i] = board[i];
+        }
+    }
 }
--- src/com/shaunofthelive/MentalBlox/views/ConsoleGameView.java
+++ src/com/shaunofthelive/MentalBlox/views/ConsoleGameView.java
@@ -5,12 +5,20 @@ import com.shaunofthelive.MentalBlox.models.Game;
 
 public class ConsoleGameView implements IGameView {
     private ConsoleBoardView boardView;
+    final int ROWS = 24;
+    final int COLS = 80;
+    char[][] g = new char [ROWS][COLS];
 
     public ConsoleGameView(IGameController controller, Game gameModel) {
         System.out.println("Console game view created");
+        this.boardView = new ConsoleBoardView();
+        draw(g);
     }
 
-    public void setBoardView(ConsoleBoardView boardView) {
-        this.boardView = boardView;
+    public void draw(char[][] g) {
+        boardView.draw(g);
+        for (int i = 0; i < g.length; ++i) {
+            System.out.println(new String(g[i]));
+        }
     }
 }
